824664785cd21e261b16366afcd48cd118efe5dd
meltedelement <meltedelement@gmail.com>
2024-11-21 11:15:35 +0000

    halfway through my bracket remover

--- EPIC/src/App.java
+++ EPIC/src/App.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+import java.util.Arrays; 
+
 public class App {
+    int openBracketLocation;
+    int closeBracketLocation;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter an expression to be evaluated!");
+        String userIn = scan.nextLine();
+
+
+        bracketSplitup(userIn)
+
     }
+
+
+    public String[] bracketSplitup(String[] expressionSplit){
+        boolean bracketFound = false;
+        for (String x : expressionSplit){
+            if (x == "("){
+                bracketFound = true;
+                openBracketLocation = Arrays.binarySearch(expressionSplit, x);
+            }
+            else if (x == ")"){
+                bracketFound = true;
+
+                closeBracketLocation = Arrays.binarySearch( expressionSplit, x);
+                break;
+            }
+        }
+        if (bracketFound){
+            return(bracketSplitup(getSliceOfArray(expressionSplit, openBracketLocation, closeBracketLocation)))
+        }
+        return(expressionSplit);
+    }
+
+    public static String[] getSliceOfArray(String[] arr, int start,  int end){
+
+    
+    String[] slice = new String[end - start];
+
+    // Copy elements of arr to slice
+    for (int i = 0; i < slice.length; i++) {
+        slice[i] = arr[start + i];
+    }
+
+    // return the slice
+    return slice;
+}
 }
