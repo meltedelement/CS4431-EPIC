6386f088ccb39fffcd1eef2c7942bab4603c9643
fitzylala <skye.fitzpatrick100@gmail.com>
2024-12-16 19:46:57 +0000

    OOO!

--- EPIC/src/Menu.java
+++ EPIC/src/Menu.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class Menu {
+
+    public static void clearScreen() {
+        System.out.print("\033\143");
+    }
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n\t ________       ________       ________          ___");
+        System.out.println("\t|\\   __  \\     |\\   __  \\     |\\   __  \\        |\\  \\      ");
+        System.out.println("\t\\ \\  \\|\\  \\    \\ \\  \\|\\  \\    \\ \\  \\|\\  \\       \\ \\  \\ ");
+        System.out.println("\t \\ \\  \\\\\\  \\    \\ \\  \\\\\\  \\    \\ \\  \\\\\\  \\       \\ \\  \\   ");
+        System.out.println("\t  \\ \\  \\\\\\  \\    \\ \\  \\\\\\  \\    \\ \\  \\\\\\  \\       \\ \\__\\   ");
+        System.out.println("\t   \\ \\_______\\    \\ \\_______\\    \\ \\_______\\       \\|__|   ");
+        System.out.println("\t    \\|_______|     \\|_______|     \\|_______|           ___ ");
+        System.out.println("\t                                                      |\\__\\");
+        System.out.println("  Welcome to the Order of Operations (OOO!) Calculator    \\|__|\n");
+
+        while (1 == 1) {
+            System.out.print("Please enter an expression to be evaluated: ");
+            String str = input.nextLine();
+            while (!Validation.isValid(str)) {
+                System.out.println("Please try again, enter an expression to be evaluated: ");
+                str = input.nextLine();
+            }
+
+
+        }
+    }
+
 }
