14df5a309f19c26b2234f4250e76c634a36c460c
fitzylala <skye.fitzpatrick100@gmail.com>
2024-12-17 19:31:26 +0000

    Fixing issues with merge

--- EPIC/src/Menu.java
+++ EPIC/src/Menu.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
@@ -6,10 +7,6 @@ public class Menu {
         System.out.print("\033\143");
     }
 
-<<<<<<< HEAD
-    public static void main(String args[]) {
-        Scanner input = new Scanner(System.in);
-=======
     public static void runCalculator(Scanner input) throws InterruptedException {
         System.out.print("Please enter an expression to be evaluated: ");
         String str = input.nextLine();
@@ -27,7 +24,6 @@ public class Menu {
     }
 
     public static void main(String args[]) throws InterruptedException {
->>>>>>> c1b1c74815abcd8a3384bacd4eacdc7ace87967f
         System.out.println("\n\t ________       ________       ________          ___");
         System.out.println("\t|\\   __  \\     |\\   __  \\     |\\   __  \\        |\\  \\      ");
         System.out.println("\t\\ \\  \\|\\  \\    \\ \\  \\|\\  \\    \\ \\  \\|\\  \\       \\ \\  \\ ");
@@ -38,21 +34,31 @@ public class Menu {
         System.out.println("\t                                                      |\\__\\");
         System.out.println("  Welcome to the Order of Operations (OOO!) Calculator    \\|__|\n");
 
-        while (1 == 1) {
-            System.out.print("Please enter an expression to be evaluated: ");
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("\t MAIN MENU");
+            System.out.printf("%s%n%s%n%s%n", "\t\t1) OOO Informational Calculator",
+                    "\t\t2) Advanced mode (matrices)", "\t\t3) Exit");
+
+            System.out.print("Enter a menu choice: ");
             String str = input.nextLine();
-            while (!Validation.isValid(str)) {
-                System.out.print("Please try again, enter an expression to be evaluated: ");
+            while (!(str.equals("1") | str.equals("2") | str.equals("3")) ) {
+                System.err.println("Invalid option. Please try again.");
+                Thread.sleep(500);
+                System.out.print("Enter a menu choice: ");
                 str = input.nextLine();
             }
-            System.out.println("Let's start our calculations!\n");
-            HandleBrackets handleBrackets = new HandleBrackets();
-            while (str.contains("(")){
-                str = handleBrackets.bracketSplitup(str);
+            switch (str) {
+                case "1": runCalculator(input); break;
+                case "2":MatrixCalculator.main(new String[]{}); break;
+                case "3":
+                    System.out.println("Thank you for using the OOO calculator!\nBye!! :)");
+                    return;
             }
-            double result = ParseStringExpression.evaluate(str);
-            System.out.println("\n...and it's as easy as that! Our result is " + result + "\n");
+            clearScreen();
+
         }
+
     }
 
 }
