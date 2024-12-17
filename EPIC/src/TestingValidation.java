06882123bce7c363142a2250e083faca744bf09b
fitzylala <skye.fitzpatrick100@gmail.com>
2024-12-17 18:24:09 +0000

    Testing hollys thing Woooo go Holly

--- EPIC/src/TestingValidation.java
+++ EPIC/src/TestingValidation.java
@@ -3,28 +3,20 @@ public class TestingValidation {
     public static void main(String[] args) {
 
         // test cases where validation is wrong
-        //System.out.println(Validation.isValid("")); // false but says true
-        // System.out.println(Validation.isValid("4")); // false but says true
-        //System.out.println(Validation.isValid("()")); // false but says true
+        System.out.println(": " + Validation.isValid("")); // false and wont run
+        //System.out.println("(): " + Validation.isValid("()")); // false and wont run
 
-        //fixed
-        // System.out.println("\n5/0 <- false"); //not a problem for validation - fixed in caluclate
-        // System.out.println(Validation.isValid("5/0")); // false but says true
-        // System.out.println("\n2.2*5 <- true"); //fixed
-        // System.out.println(Validation.isValid("2.2*5")); // true but says false
-        // System.out.println("\n5/(3-3) <- false"); //zero error again caught elsewhere
-        // System.out.println(Validation.isValid("5/(3-3)")); // false but says true
-        // System.out.println("\n1.324654353+ 5645767657 <- true"); //fixed
-        // System.out.println(Validation.isValid("1.324654353+ 5645767657")); // true but says false   //fix by add '.' to arry list
-        // System.out.println("\n\n\n");
+        System.out.println("5/0: " + Validation.isValid("5/0")); // false but division by zero error is handled elsewhere
+        System.out.println("5/(3-3): " + Validation.isValid("5/(3-3)")); // false but division by zero error is handled elsewhere
 
-        // test cases where validation is correct
-        //System.out.println(Validation.isValid("(()")); // false
-    //     System.out.println(Validation.isValid("{5*12}")); // false
-    //     System.out.println(Validation.isValid("=2/3+2")); // false
-    //     System.out.println(Validation.isValid("3+65+2+1")); // true
-    //     System.out.println(Validation.isValid("((2)(3))")); // true but returns false as no operator.
-
-    //     System.out.println(Validation.isValid("3^2*2-12^-(1/2)")); // true
+        System.out.println("4: " + Validation.isValid("4")); // false
+        System.out.println("((): " + Validation.isValid("(()")); // false
+        System.out.println("2.2*5: " + Validation.isValid("2.2*5")); // true
+        System.out.println("{5*12}: " + Validation.isValid("{5*12}")); // false
+        System.out.println("=2/3+2: " + Validation.isValid("=2/3+2")); // false
+        System.out.println("3+65+2+1: " + Validation.isValid("3+65+2+1")); // true
+        System.out.println("((2)(3)): " + Validation.isValid("((2)(3))")); // true
+        System.out.println("3^2*2-12^-(1/2): " + Validation.isValid("3^2*2-12^-(1/2)")); // true
+        System.out.println("1.324654353+ 5645767657: " + Validation.isValid("1.324654353+ 5645767657")); // true
     }
 }
