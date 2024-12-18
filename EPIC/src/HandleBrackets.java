d658d06eebfeff34b52bfc7902ee914bc35c3c1e
fitzylala <skye.fitzpatrick100@gmail.com>
2024-12-18 15:36:40 +0000

    Return of Appy

--- EPIC/src/HandleBrackets.java
+++ EPIC/src/HandleBrackets.java
@@ -41,11 +41,11 @@ public class HandleBrackets {
     }
     
     public static void main(String[] args){
-        HandleBrackets handleBrackets = new HandleBrackets();
+        HandleBrackets appy = new HandleBrackets();
         String test = "(3 + 2)(8 * 9)";
-        test = handleBrackets.bracketSplitup(test);
+        test = appy.bracketSplitup(test);
         while (test.contains("(")){
-            test = handleBrackets.bracketSplitup(test);
+            test = appy.bracketSplitup(test);
         }
         System.out.println(ParseStringExpression.evaluate(test));
     }
